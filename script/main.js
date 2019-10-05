4d61a0b167e6555b3fb91b5fb710dc3bdb6a7f90
Vincente Squitiro <blueberrybark@gmail.com>
2019-10-05 02:29:18 -0400

    Add first attempt at slime object

--- script/main.js
+++ script/main.js
@@ -15,8 +15,8 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload () {
-    this.load.image('staticSlime','assets/slime.png');
-    this.load.spritesheet('slime','assets/slimeTiles.png',
+    //this.load.image('staticSlime','assets/slime.png');
+    this.load.spritesheet('mass','assets/slimeTiles.png',
         {frameWidth: 32, frameHeight: 32});
     this.load.image('border','assets/bordertileset.png', 
         {frameWidth: 32, frameHeight: 32});
@@ -27,12 +27,36 @@ function preload () {
 var workers;
 var allSlimes;
 
+var colors = [0xffffff,0xc0c0c0,0x808080,0x000000,0xff0000,0x800000,0xffff00,0x808000,0x00ff00,0x008000,0x00ffff,0x008080,0x0000ff,0x000080,0xff00ff,0x800080];
+
+class Slime {
+    constructor(ID,RoomID,traitor) {
+        this.ID = ID;
+        this.RoomID = RoomID;
+        this.xPos = Math.random()*280+10;
+        this.yPos = Math.random()*180+10;
+        //this.eyes = 1;
+        //this.mouth = 1;
+        //this.hair = 1;
+        this.tint = colors[2];
+        this.traitor = traitor;
+        this.test = "Test";
+        this.sprite;
+    }
+
+    setSprite(sprite) {
+        this.sprite = sprite;
+    }
+}
+
+var classySlime = new Slime(0,0,false);
+
 function create () {
 
     var config = {
         key: 'wiggle',
-        frames: this.anims.generateFrameNumbers('slime', {start: 0, end: 1, first:0}),
-        frameRate: 30,
+        frames: this.anims.generateFrameNumbers('mass', {start: 0, end: 1, first:0}),
+        frameRate: 5,
         repeat: -1,
         repeatDelay: 2
     };
@@ -43,15 +67,18 @@ function create () {
         var x = Phaser.Math.Between(10,790);
         var y = Phaser.Math.Between(10,590);
 
-        allSlimes = this.add.sprite(x,y, 'slime', 1);
+        allSlimes = this.add.sprite(x,y, 'mass', 1);
 
         allSlimes.anims.delayedPlay(Math.random() * 3, 'wiggle');
     }
 
-    workers = this.add.sprite(400,300,'slime');
+    classySlime.setSprite(this.add.sprite(300,300,'mass'));
+    classySlime.sprite.setTint(classySlime.tint);
+
+    workers = this.add.sprite(400,300,'mass');
     this.anims.create({
         key: 'bounce',
-        frames: this.anims.generateFrameNumbers('slime', {start: 0, end: 1}),
+        frames: this.anims.generateFrameNumbers('mass', {start: 0, end: 1}),
         frameRate: 5,
         repeat: -1
     })
@@ -61,4 +88,5 @@ function create () {
 function update () {
     workers.anims.play('bounce',true);
     allSlimes.anims.play('bounce',true);
+    classySlime.sprite.play('bounce',true);
 }
