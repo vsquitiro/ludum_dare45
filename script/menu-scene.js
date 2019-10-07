64b4286b73f5f9960f17510df42ede23a80ef9d4
Chris Rock <chris@abysmalsoft.org>
2019-10-06 21:36:04 -0400

    Add shortcut to menu

--- script/menu-scene.js
+++ script/menu-scene.js
@@ -12,12 +12,20 @@ class MenuScene extends Phaser.Scene {
         var text = this.add.text(screenWidth/2, screenHeight/2, 'Click Anywhere to Start');
         text.setOrigin(0.5, 0.5);
 
+        this.shortcutZone = this.add.zone(24 * 32, 18 * 32, 1 * 32, 1 * 32).setOrigin(0).setName('shortcut');
+        this.shortcutZone.setInteractive({cursor: 'pointer'});
+
+        this.input.on('gameobjectdown', function(pointer, gameObject) {
+            if (gameObject.name == "shortcut") {
+                SystemState.shortcut();
+            }
+        }, this);
         this.input.once('pointerdown', function() {
             console.log("Clicked, starting game");
 
-            //SystemState.gameStart();
-            //SystemState.debugTest();
-            SystemState.shortcut();
+            if (SystemState.state == "menu") {
+                SystemState.gameStart();
+            }
         }, this);
     }
 }
